491c0c5c3e594351e73fc1f4016b535756329861
fihill <faylien@naver.com>
2025-01-14 09:50:31 +0900

    initial commit

---
+++ src/app/param/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function Param({ params }: { params: { id: string } }) {
+  return <div>Param: {params.id}</div>;
+}
---
+++ src/app/param/page.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Param() {
+  return <div>Param</div>;
+}
